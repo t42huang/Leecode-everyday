4ca31d33325af65e3cb03cb47b193b458032d9d1
Tina Huang <t42huang@gmail.com>
2020-12-08 01:10:34 -0500

    946. Validate Stack Sequences

---
+++ 946. Validate Stack Sequences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0, j = 0; i < pushed.length; i++) {
+            stack.push(pushed[i]);
+            
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                j++;
+                stack.pop();
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
